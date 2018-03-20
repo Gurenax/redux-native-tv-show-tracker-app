961cbfaf852337c3561538d5a59887fb806408e9
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-21 10:17:59 +1100

    Initialise project with create-react-native and native-base

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { Container } from 'native-base'
+
+export default class App extends React.Component {
+  render() {
+    return <Container />
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
