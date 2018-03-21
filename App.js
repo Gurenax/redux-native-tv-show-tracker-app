6e462d794669c4aea6fd96ecf10b19f66c63f65a
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-21 12:31:32 +1100

    Configure App to use Redux/Redux Saga

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 
-import { Container } from 'native-base'
+/*
+* Redux
+*/
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+/*
+* Redux Saga
+*/
+import createSagaMiddleware from 'redux-saga'
+import showListSaga from './sagas/showListSaga'
+const sagaMiddleware = createSagaMiddleware()
+// import { Container, Content, Footer, Header, Item, Input, Icon, Button, Text } from 'native-base'
+
+/*
+* Screens
+*/
+import ShowList from './screens/ShowList'
+
+/*
+* Create a Redux Store for the RootReducer (needed by Provider)
+*/
+import RootReducer from './reducers/rootReducer'
+const store = createStore(RootReducer, applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(showListSaga)
 
 export default class App extends React.Component {
   render() {
-    return <Container />
+    return (
+      <Provider store={store}>
+        <ShowList />
+      </Provider>
+    )
   }
 }
 
